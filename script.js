3f2dbcd88de9d2b30cfc767fb1a9b50a6eda469b
roman-thapa <romanthapa7795@gmail.com>
2022-10-25 17:58:05 +0530

    Make onclick function for all numbers

--- script.js
+++ script.js
@@ -1,1 +1,61 @@
-console.log("Calculator");
+let firstNumber;
+let secondNumber;
+
+let entry = document.querySelector('.secondDisplay');
+let result = document.querySelector('.mainDisplay');
+
+
+let one = document.querySelector('.num1');
+one.addEventListener('click', () => {
+    console.log('success');
+}); 
+
+let two = document.querySelector('.num2');
+two.addEventListener('click', () => {
+    console.log('success');
+}); 
+
+let three = document.querySelector('.num3');
+three.addEventListener('click', () => {
+    console.log('success');
+}); 
+
+let four = document.querySelector('.num4');
+four.addEventListener('click', () => {
+    console.log('success');
+}); 
+
+let five = document.querySelector('.num5');
+five.addEventListener('click', () => {
+    console.log('success');
+}); 
+
+let six = document.querySelector('.num6');
+six.addEventListener('click', () => {
+    console.log('success');
+}); 
+
+let seven = document.querySelector('.num7');
+seven.addEventListener('click', () => {
+    console.log('success');
+}); 
+
+let eight = document.querySelector('.num8');
+eight.addEventListener('click', () => {
+    console.log('success');
+}); 
+
+let nine = document.querySelector('.num9');
+nine.addEventListener('click', () => {
+    console.log('success');
+}); 
+
+let zero = document.querySelector('.num0');
+zero.addEventListener('click', () => {
+    console.log('success');
+}); 
+
+let point = document.querySelector('.point');
+point.addEventListener('click', () => {
+    console.log('success');
+}); 
